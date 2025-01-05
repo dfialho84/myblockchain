581c932abcd1f63ab669c566b8c6c6e3f6e9dafc
Diego Fialho Rodrigues <diego.fialho@ufv.br>
2025-01-05 10:28:38 -0300

    code coverage adjsutments

--- services/cryptochain/jest.config.ts
+++ services/cryptochain/jest.config.ts
@@ -22,13 +22,13 @@ const config: Config = {
     collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    collectCoverageFrom: ["**/*.ts"],
+    collectCoverageFrom: ["src/**/*.ts"],
 
     // The directory where Jest should output its coverage files
     coverageDirectory: "coverage",
 
     // An array of regexp pattern strings used to skip coverage collection
-    coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
+    coveragePathIgnorePatterns: ["/node_modules/", "/dist/", "/src/types/"],
 
     // Indicates which provider should be used to instrument code for coverage
     coverageProvider: "v8",
